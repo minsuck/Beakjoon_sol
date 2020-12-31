9164c3ec8328ae8897726f364405f4e908075aaf
036ab@naver.com <31887989+minsuck@users.noreply.github.com>
2020-12-31 13:42:37 +0900

    A+B를 조금 더 아름답게 출력하는 문제

---
+++ 03_for문/11021.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int number_of_case;
+    int num_front, num_back;
+
+    scanf("%d", &number_of_case);
+
+    for(int i=0;i<number_of_case;i++){
+        scanf("%d %d", &num_front, &num_back);
+        printf("Case #%d: %d\n", i+1, num_front+num_back);
+    }
+    return 0;
+}
